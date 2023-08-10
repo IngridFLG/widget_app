2fb6c7539df4b18df1ab019200601017010686f7
Ingrid Florez <ingrid.florez0226@gmail.com>
2023-08-10 17:20:19 -0500

    se implementa el app theme

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const colorList = <Color> [
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.pink,
+  Colors.purple
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme(this.selectedColor);
+
+
+}
