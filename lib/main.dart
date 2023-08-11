ee2ca46ac6503f0b1ce007d26ba039ea2b698ffc
Ingrid Florez <ingrid.florez0226@gmail.com>
2023-08-11 13:58:06 -0500

    go router configurado

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/router/app_router.dart';
 import 'package:widgets_app/config/theme/app_theme.dart';
-import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
-import 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
-import 'package:widgets_app/presentation/screens/home/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,15 +12,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      //el router para mostrar los screen
+      routerConfig: appRouter,
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme:AppTheme(selectedColor: 3).getTheme(),
-      home: const HomeScreen(),
-      routes: {
-        '/buttons' : (context) => const ButtonsScreen(),
-        '/cards' : (context) => const CardScreen(),
-      },
+      
       
     );
   }
