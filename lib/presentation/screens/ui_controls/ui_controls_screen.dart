4fbaa1a6e062a3ad948f7baa42d0a6f3770f9bec
Ingrid Florez <ingrid.florez0226@gmail.com>
2023-08-13 10:22:34 -0500

    pantallas creadas

---
+++ lib/presentation/screens/ui_controls/ui_controls_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class UiControlScreen extends StatelessWidget {
+
+  static const name = 'uicontrol_screen';
+  
+  const UiControlScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
